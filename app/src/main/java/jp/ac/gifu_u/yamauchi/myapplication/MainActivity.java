c84dc73ea45ea40c04979a89b933a08b8b86d959
Yamauchi Mana <yamauchi.mana.c7@s.gifu-u.ac.jp>
2024-05-01 16:49:04 +0900

    5/1 commit

--- app/src/main/java/jp/ac/gifu_u/yamauchi/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/yamauchi/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package jp.ac.gifu_u.yamauchi.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // button1 がクリックされたときの処理
+                Log.d("ButtonClick", "OK");
+            }
+        });
+
     }
 }
