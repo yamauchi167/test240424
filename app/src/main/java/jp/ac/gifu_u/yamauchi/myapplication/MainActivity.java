5bb0085564fdbea0573a7de12595e165251fd4c2
Yamauchi Mana <yamauchi.mana.c7@s.gifu-u.ac.jp>
2024-05-08 09:45:14 +0900

    5/8-1 commit

--- app/src/main/java/jp/ac/gifu_u/yamauchi/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/yamauchi/myapplication/MainActivity.java
@@ -1,26 +1,30 @@
 package jp.ac.gifu_u.yamauchi.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.util.Log;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button4 = findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        //setContentView(R.layout.activity_main);
+        setContentView(new MyView(this));
+        //Button button4 = findViewById(R.id.button4);
+        //button4.setOnClickListener(new View.OnClickListener() {
+
+            /*public void onClick(View v) {
                 // button1 がクリックされたときの処理
                 Log.d("ButtonClick", "OK");
-            }
-        });
-
+            }*/
     }
 }
---
+++ app/src/main/java/jp/ac/gifu_u/yamauchi/myapplication/MyView.java
@@ -0,0 +1,23 @@
+package jp.ac.gifu_u.yamauchi.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MyView extends View {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.BLUE);
+
+        canvas.drawLine(10, 20, 30, 40, paint);
+    }
+}
